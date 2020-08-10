ff481bf0d855ad9d663fb00509853db7cfd84ec1
swift1719 <ayushpawar1501@gmail.com>
2020-08-10 23:57:04 +0530

    Updates

---
+++ precache-manifest.cf53960abdd96bf2bc44c57f2b54e654.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bc4a1db1431aa4d4a6e2719ea98579e",
+    "url": "/swift1719/weatherApp_usingReact/index.html"
+  },
+  {
+    "revision": "c90714b440bf9f34d741",
+    "url": "/swift1719/weatherApp_usingReact/static/css/2.14c93bbb.chunk.css"
+  },
+  {
+    "revision": "692fa9222328ffd0153c",
+    "url": "/swift1719/weatherApp_usingReact/static/css/main.dc39f6db.chunk.css"
+  },
+  {
+    "revision": "c90714b440bf9f34d741",
+    "url": "/swift1719/weatherApp_usingReact/static/js/2.11834bb4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/swift1719/weatherApp_usingReact/static/js/2.11834bb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "692fa9222328ffd0153c",
+    "url": "/swift1719/weatherApp_usingReact/static/js/main.86af8173.chunk.js"
+  },
+  {
+    "revision": "e8d60d74d44831397b3b",
+    "url": "/swift1719/weatherApp_usingReact/static/js/runtime-main.d3c89ef2.js"
+  },
+  {
+    "revision": "453135b2613152ca7b7b81e2abfea108",
+    "url": "/swift1719/weatherApp_usingReact/static/media/cov1.453135b2.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/swift1719/weatherApp_usingReact/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/swift1719/weatherApp_usingReact/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/swift1719/weatherApp_usingReact/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/swift1719/weatherApp_usingReact/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/swift1719/weatherApp_usingReact/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
